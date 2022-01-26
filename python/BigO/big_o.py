12c8d1ed70363361972ff7ee99bcfa0d234a69e4
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-25 23:04:17 -0300

    chore: code style

--- python/BigO/big_o.py
+++ python/BigO/big_o.py
@@ -1,30 +1,34 @@
 # O(n)
 # se n = 10, a função tem 11 passos
-# complexidade linear 
+# complexidade linear
 # quanto maior o input de dados, maior o tempo de execução.
 
+
 def soma(n):
-  soma = 0
-  for i in range(n + 1):
-    soma += i
-  
-  return soma
+    soma = 0
+    for i in range(n + 1):
+        soma += i
+
+    return soma
+
 
 # O(n)
 # quanto maior o input de dados, maior o tempo de execução.
 def lista1():
-  lista = []
-  for i in range(1000):
-    lista += i
-  return lista
+    lista = []
+    for i in range(1000):
+        lista += i
+    return lista
+
 
 # O(1)
 # sempre 3 passos
-# complexidade constante 
+# complexidade constante
 # o tamanho do input não interfere no tempo de execução.
 def soma_linear(n):
-  return (n * (n + 1)) / 2
+    return (n * (n + 1)) / 2
+
 
 # O(1)
 def lista2():
-  return range(1000)
+    return range(1000)
