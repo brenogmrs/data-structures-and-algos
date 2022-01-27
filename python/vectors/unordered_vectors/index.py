ca0dd1f4959f9362b1f5eb06e40af0c89cdad952
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-26 22:15:13 -0300

    add insert function to the vector

--- python/vectors/unordered_vectors/index.py
+++ python/vectors/unordered_vectors/index.py
@@ -2,10 +2,10 @@ import numpy as np
 
 
 class UnorderedVector:
-    def __init__(self, capability):
-        self.capability = capability
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.last_position = -1
-        self.values = np.empty(self.capability, dtype=int)
+        self.values = np.empty(self.capacity, dtype=int)
 
     # O(n)
     def print_vector(self):
@@ -15,7 +15,20 @@ class UnorderedVector:
             for i in range(self.last_position + 1):
                 print(i, "-", self.values[i])
 
+    # O(1)
+    def insert(self, value):
+        if self.last_position == self.capacity - 1:
+            print("limit capacity hit")
+        else:
+            self.last_position += 1
+            self.values[self.last_position] = value
+
 
 vector = UnorderedVector(5)
+vector.insert(3)
+vector.insert(5)
+vector.insert(8)
+vector.insert(1)
+vector.insert(7)
 
 vector.print_vector()
