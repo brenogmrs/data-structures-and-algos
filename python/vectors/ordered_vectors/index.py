7b3edb73049642a28e7b88c5a5b3e2ea59fa3904
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-29 12:06:13 -0300

    add function to add ordered values to vector

--- python/vectors/ordered_vectors/index.py
+++ python/vectors/ordered_vectors/index.py
@@ -8,11 +8,48 @@ class OrderedVector:
         self.values = np.empty(self.capacity, dtype=int)
 
     # O(n)
-    def print_vector(self):
+    def print(self):
         if self.last_position == -1:
             print("The vector is empty")
         else:
-            vector = []
+            print("-----")
             for i in range(self.last_position + 1):
-                vector.append(self.values[i])
-            print(vector)
+                print(i, "-", self.values[i])
+            print("-----")
+
+    # O(n)
+    def add(self, value):
+        if self.last_position == self.capacity - 1:
+            print("limit capacity hit")
+            return
+
+        position = 0
+        for i in range(self.last_position + 1):
+            position = i
+            if self.values[i] > value:
+                break
+            if i == self.last_position:
+                position = i + 1
+
+        x = self.last_position
+        while x >= position:
+            self.values[x + 1] = self.values[x]
+            x -= 1
+
+        self.values[position] = value
+        self.last_position += 1
+
+
+vector = OrderedVector(10)
+
+vector.add(6)
+vector.add(4)
+vector.add(3)
+vector.add(5)
+vector.add(2)
+vector.add(4)
+vector.add(2)
+vector.add(4)
+vector.add(5)
+
+vector.print()
