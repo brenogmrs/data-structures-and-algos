62bb40a16879b9b8fbfc713ac6b7d93008ffa2d5
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-02-28 19:33:52 -0300

    add linear search

--- typescript/vectors/ordered_vectors.ts
+++ typescript/vectors/ordered_vectors.ts
@@ -1,5 +1,70 @@
-function teste(teste: string): string {
-  return teste;
-}
+class OrderedVector {
 
-console.log(teste('teste'));
+  capacity: number
+  lastPosition: number
+  values: number[]
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.lastPosition = -1;
+    this.values = [];
+  }
+
+
+  public print() {
+    if(this.lastPosition === -1){
+      return 'the vector is empty';
+    }else {
+      for(let i = 0; i <= this.lastPosition + 1; i++){
+        console.log(i, "-", this.values[i])
+      }
+    }
+  }
+
+  public add(value: number) {
+    if(this.lastPosition === this.capacity - 1){
+      console.log("hit limit capacity")
+      return
+    }
+
+    let positionToPlaceValue = 0;
+
+    for(let i = 0; i <= this.lastPosition + 1; i++){
+      positionToPlaceValue = i
+
+      if(this.values[i] > value) {
+        break
+      }
+
+      if(i === this.lastPosition) {
+        positionToPlaceValue = i + 1
+      }
+
+    }
+
+    let vectorsLastPosition = this.lastPosition
+    while(vectorsLastPosition >= positionToPlaceValue){
+      this.values[vectorsLastPosition + 1] = this.values[vectorsLastPosition]
+      vectorsLastPosition -= 1
+    }
+
+    this.values[positionToPlaceValue] = value
+    this.lastPosition += 1
+    
+  }
+
+  public linearSearch(value: number) {
+    for(let i = 0; i < this.lastPosition + 1; i++) {
+      if (this.values[i]) {
+        return -1
+      }
+      if(this.values[i] === value){
+        return i
+      }
+      if(i === this.lastPosition){
+        return -1
+      }
+    }
+  }
+
+}
